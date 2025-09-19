a2ab2a78688f1e48e4a71a6d48ae7ae564ddfddb
elviredev <sandrine.rodriguez@hotmail.com>
2025-09-19 18:34:56 +0200

    🐽 Mise en place Page dashboard (Layout & Partials)

---
+++ resources/views/back/partials/styles.blade.php
@@ -0,0 +1,15 @@
+<link
+  rel="shortcut icon"
+  type="image/x-icon"
+  href="{{ asset('back_auth/assets/img/logo.png') }}"
+/>
+<link rel="stylesheet" href="{{ asset('back_auth/assets/css/bootstrap.min.css') }}" />
+<link
+  rel="stylesheet"
+  href="{{ asset('back_auth/assets/plugins/fontawesome/css/fontawesome.min.css') }}"
+/>
+<link rel="stylesheet" href="{{ asset('back_auth/assets/plugins/fontawesome/css/all.min.css') }}" />
+<link rel="stylesheet" href="{{ asset('back_auth/assets/css/feathericon.min.css') }}" />
+<link rel="stylehseet" href="https://cdn.oesmith.co.uk/morris-0.5.1.css" />
+<link rel="stylesheet" href="{{ asset('back_auth/assets/plugins/morris/morris.css') }}" />
+<link rel="stylesheet" href="{{ asset('back_auth/assets/css/style.css') }}" />
